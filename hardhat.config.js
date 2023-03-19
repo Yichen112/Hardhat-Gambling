b527369f833e7ed26b578fabfe13ac5403bb12cb
Yichen112 <100495438+Yichen112@users.noreply.github.com>
2023-03-19 19:41:32 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
-/** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.17",
+  contracts: {
+    paths: ["contracts"],
+    artifactsDir: "artifacts",
+    // Add the following lines to compile multiple solidity files
+    // relative to the contracts folder
+    glob: [
+      "hw_2_Challenge_Engine.sol",
+      "hw_2_Challenge_Game.sol"
+    ]
+  }
 };
+
